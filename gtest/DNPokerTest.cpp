c6afceb0516c75c05b8774b1276cc20293cf4b54
hongyuwang1993 <1850323612@qq.com>
2014-07-09 10:19:20 +0800

    add testEqual

--- gtest/DNPokerTest.cpp
+++ gtest/DNPokerTest.cpp
@@ -17,8 +17,39 @@ protected:
 		EXPECT_EQ(0, poker1.poker_color);
 		EXPECT_EQ(0, poker1.poker_value);
 	}
-};
+
+	void testEqual(){
+		struct DNPoker poker1;
+		struct DNPoker poker2;
+		poker1.poker_color = 0;
+		poker1.poker_value = 2;
+		poker2.poker_color = 3;
+		poker2.poker_value = 0;
+		if (poker1.IsNullPoker()){
+			if (poker2.IsNullPoker())
+				EXPECT_TRUE(poker1 == poker2);
+			else
+				EXPECT_FALSE(poker1 == poker2);
+		}
+		poker2.poker_value = 1;
+		if (poker1.IsNullPoker()){
+			if (poker2.IsNullPoker())
+				EXPECT_TRUE(poker1 == poker2);
+			else
+				EXPECT_FALSE(poker1 == poker2);
+		}
+		poker1.poker_color = 2;
+		EXPECT_FALSE(poker1 == poker2);
+		poker1.poker_color = 3;
+		poker1.poker_value = 1;
+		EXPECT_TRUE(poker1 == poker2);
+	}
+}; 
 
 TEST_F(DNPokerTest, testConstructor) {
 	testConstructor();
+}
+
+TEST_F(DNPokerTest, testEqual) {
+	testEqual();
 }
